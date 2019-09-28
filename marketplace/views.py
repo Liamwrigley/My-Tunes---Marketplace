2d7f59c36df02038a7182ae219c1ee0bf7386ea5
Liam <Wrigley>
2019-09-28 20:14:22 +1000

    Began templating pages and adding workflow to login/register routes

--- marketplace/views.py
+++ marketplace/views.py
@@ -4,15 +4,7 @@ bp = Blueprint('main', __name__, template_folder="templates")
 
 # https://hackersandslackers.com/the-art-of-building-flask-routes
 
-@bp.route('/')
+@bp.route('/', endpoint="home")
 def home():
-  # adding index.html - need to make into a template
-    return render_template('index.html')
-
-@bp.route('/login')
-def login():
-    return '<h1>LOGIN<h1>'
-
-@bp.route('/logout')
-def logout():
-    return redirect('/login')
+  #listing = Listing.query.all()
+    return render_template('index.html') #add ,listing=listing

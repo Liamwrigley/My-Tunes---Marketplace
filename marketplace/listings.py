5f449a52f5ae1b59cfef44c71b0265396f476f1b
Liam Wrigley <liamwrigley01@gmail.com>
2019-10-02 13:14:15 +1000

    added 404 error handling. Added connection between listing and user.

--- marketplace/listings.py
+++ marketplace/listings.py
@@ -1,6 +1,6 @@
-from flask import ( 
+from flask import (
     Blueprint, flash, render_template, request, url_for, redirect
-) 
+)
 from .models import Listing,User #add Comment back later
 from .forms import ListingForm #add comment form later
 from . import db
@@ -10,24 +10,25 @@ from flask_login import login_required, current_user
 bp = Blueprint('listing', __name__, url_prefix='/listings')
 
 #create a page that will show the details fo the destination
-@bp.route('/<id>') 
-def show(id): 
-  listing = Listing.query.filter_by(id=id).first()  
+@bp.route('/<int:id>')
+def show(id):
+  listing = Listing.query.filter_by(id=id).first()
   # cform = CommentForm()
   return render_template('listings/show.html', listing=listing, form=cfor)
 
 
 @bp.route('/create', methods = ['GET', 'POST'])
-@login_required   #decorator between the route and view function 
+@login_required   #decorator between the route and view function
 def create():
   form = ListingForm()
   if form.validate_on_submit():
     # on successful validation add data
-    listing = Listing(name=form.name.data, 
+    listing = Listing(name=form.name.data,
                 description=form.description.data,
                 image=form.image.data,
                 price=form.currency.data,
-                genre=form.genre.data)
+                genre=form.genre.data,
+                owner_id=current_user) #Will need to check the output of current_user
     # push to db
     db.session.add(listing)
     db.session.commit()
@@ -40,23 +41,22 @@ def create():
 
 
 
-# @bp.route('/<destination>/comment', methods = ['GET', 'POST'])  
+# @bp.route('/<destination>/comment', methods = ['GET', 'POST'])
 # @login_required
-# def comment(destination):  
-#     form = CommentForm()  
+# def comment(destination):
+#     form = CommentForm()
 #     #get the destination object associated to the page and the comment
-#     destination_obj = Destination.query.filter_by(id=destination).first()  
-#     if form.validate_on_submit():  
+#     destination_obj = Destination.query.filter_by(id=destination).first()
+#     if form.validate_on_submit():
 #       #read the comment from the form
-#       comment = Comment(text=form.text.data,  
-#                         destination=destination_obj, user=current_user) 
+#       comment = Comment(text=form.text.data,
+#                         destination=destination_obj, user=current_user)
 #       #here the back-referencing works - comment.destination is set
 #       # and the link is created
-#       db.session.add(comment) 
-#       db.session.commit() 
+#       db.session.add(comment)
+#       db.session.commit()
 
 #       #flashing a message which needs to be handled by the html
-#       flash('Your comment has been added', 'success')  
+#       flash('Your comment has been added', 'success')
 #     # using redirect sends a GET request to destination.show
 #     return redirect(url_for('destination.show', id=destination))
-

3ab95081fdea99746edefc7b001436615c512b0a
Liamwrigley <liamwrigley01@gmail.com>
2019-11-19 12:32:08 +1000

    removing load_user

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -72,21 +72,6 @@ def create_app():
         curr_user.id = user_id
         return curr_user
 
-    @login_manager.request_loader
-    def request_loader(request):
-        email = request.form.get('email')
-        if email not in users:
-            return
-
-        user = User()
-        user.id = email
-
-        # DO NOT ever store passwords in plaintext and always compare password
-        # hashes using constant-time comparison!
-        user.is_authenticated = request.form['password'] == users[email]['password']
-
-        return user
-
     # Error handing - passes through error code and template forms based on code
     @app.errorhandler(Exception)
     def handle_error(e):
